1c63fa1530fcaa9988e9442fef9d5a91584ee3e2
Cheese <swedishcheese930506@gmail.com>
2015-04-28 18:47:25 +0200

    Particle effects
    Energy recharge and jetbeam complete with prefabs, textures, materials
    and scripts

---
+++ Assets/Particle effects/Scripts/JetBeamSizeHandler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JetBeamSizeHandler : MonoBehaviour {
+
+	public ParticleSystem m_ParticleSystem;
+	public float m_StartSize;
+	public float m_StartLifetime;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		m_StartSize = 1;
+		m_StartLifetime = 0.1f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey ("space")) 
+		{
+
+			m_StartSize = 2;
+			m_StartLifetime = 0.5f;
+
+
+		} 
+		else 
+		{
+
+			m_StartSize = 1;
+			m_StartLifetime = 0.1f;
+
+		}
+		m_ParticleSystem.startSize = m_StartSize;
+		m_ParticleSystem.startLifetime = m_StartLifetime;
+	}
+}
