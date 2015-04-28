729b9449f9f156b5fd0f5fbe538389d299ed6c5f
Carveca <carveca2@gmail.com>
2015-04-28 18:58:01 +0200

    Update 2

---
+++ Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartLevel : MonoBehaviour 
+{
+	void Update () 
+    {
+        if (Input.GetKey("r"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+	}
+}
