ac70387d6ca35a8ba2f0256ce71c32cc985000d5
Carveca <carveca2@gmail.com>
2015-05-11 16:29:24 +0200

    Basic Camera Behaviour

--- Assets/Scripts/PlayerCameraBehaviour.cs
+++ Assets/Scripts/PlayerCameraBehaviour.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class PlayerCameraBehaviour : MonoBehaviour {
+public class PlayerCameraBehaviour : MonoBehaviour 
+{
+    public Transform target;
+    public float distanceUp;
+    public float distanceBack;
+    public float minimumHeight;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Not used directly:
+    private Vector3 positionVelocity;
+
+    void FixedUpdate()
+    {
+        // Calculate a new position to place the camera:
+        Vector3 newPosition = target.position + (target.forward * distanceBack);
+        newPosition.y = Mathf.Max(newPosition.y + distanceUp, minimumHeight);
+
+        // Move the camera:
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref positionVelocity, 0.18f);
+
+        // Rotate the camera to look at where the car is pointing:
+        Vector3 focalPoint = target.position + (target.forward * 5);
+        transform.LookAt(focalPoint);
+    }
+
+
 }
